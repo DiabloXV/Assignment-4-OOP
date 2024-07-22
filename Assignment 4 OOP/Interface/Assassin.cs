704160794a775d2a3af2d2bdbe7fd09ed62062ac
DiabloXV <haboali1001@gmail.com>
2024-07-22 16:24:11 +0300

    interfaces

---
+++ Assignment 4 OOP/Interface/Assassin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4_OOP.Interface
+{
+    internal class Assassin : Characterstics , Special_Skills
+    {
+        public int Health { get ; set ; }
+
+        public int Level {  get ;  }
+
+        public int BaseDamage { get ; }
+
+        public string? Weapon { get ; set ; }
+        public string? Name { get ; set; }
+
+     
+
+        public void Mount()
+        {
+            Console.WriteLine("Summon Great Serpent Mount"); 
+        }
+
+        public void Skill1()
+        {
+            Console.WriteLine("Go invisible"); 
+        }
+
+        public void Skill2()
+        {
+            Console.WriteLine("enhance your basic attack");
+        }
+
+        public void Skill3()
+        {
+            Console.WriteLine("Execute low health enimies");
+        }
+
+  
+
+        void Characterstics.SpecialMove()
+        {
+            Console.WriteLine("Dance"); ;
+        }
+
+        void Special_Skills.SpecialMove()
+        {
+            Console.WriteLine("poison enemies");
+        }
+    }
+}
--- Assignment 4 OOP/Program.cs
+++ Assignment 4 OOP/Program.cs
@@ -4,13 +4,29 @@
 
 
 
+using Assignment_4_OOP.Interface;
+
 namespace Assignment_4_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Warrior warrior = new Warrior();
+
+            Assassin assassin = new Assassin();
+
+            warrior.Name = "Ahmed";
+
+            warrior.Skill2();
+
+            Special_Skills WarriorSpecialSkill = new Warrior();
+
+            WarriorSpecialSkill.SpecialMove();
+
+            Characterstics Warrior2 = new Warrior();
+
+            Warrior2.SpecialMove();
         }
     }
 }
