d80167edde184d6c4aae4e409c4f0cc4233252ea
DiabloXV <haboali1001@gmail.com>
2024-07-22 15:03:39 +0300

    Add project files.

---
+++ Assignment 4 OOP/Interface/Characterstics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4_OOP.Interface
+{
+    internal interface Characterstics
+    {
+        public int Health { get; set; }
+
+        public int Level { get; }
+
+        public int BaseDamage { get; }
+
+        public String? Weapon { get; set; }
+
+        public String? Name { get; set; }
+
+
+        public void Skill1();
+
+        public void skill2();
+    }
+}
---
+++ Assignment 4 OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿//make an interaface and implement it 
+// implemnt it implecitly and explicitly 
+//apply all types of deep copy vs shallow copy
+
+
+
+namespace Assignment_4_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
